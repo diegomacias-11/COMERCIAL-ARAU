ff3e07707ac914925ebc851607976eddc212f526
diegomacias-11 <diegojairmacias@gmail.com>
2025-11-27 10:49:12 -0600

    Se agregaron los modelos al admin.py

--- clientes/admin.py
+++ clientes/admin.py
@@ -4,15 +4,7 @@ from .models import Cliente
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = (
-        "cliente",
-        "giro",
-        "tipo",
-        "contacto",
-        "telefono",
-        "conexion",
-        "fecha_registro",
-    )
-    list_filter = ("tipo", "giro")
-    search_fields = ("cliente", "contacto", "telefono", "conexion")
+    list_display = [field.name for field in Cliente._meta.fields]
+    search_fields = [field.name for field in Cliente._meta.fields if field.get_internal_type() in ['CharField', 'TextField']]
+    list_filter = ["tipo", "giro"]
     ordering = ("-fecha_registro",)
