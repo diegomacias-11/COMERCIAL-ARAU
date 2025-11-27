052944920a5541d4ee33389c063dbf91e7cbe120
diegomacias-11 <diegojairmacias@gmail.com>
2025-11-27 10:34:13 -0600

    Se creo el super usuario

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,21 @@ STORAGES = {
 # CLAVE PRIMARIA POR DEFECTO
 # ======================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ======================
+# CREAR SUPERUSUARIO AUTOMÁTICO EN RENDER (CONTROLADO)
+# ======================
+if os.environ.get("CREATE_SUPERUSER") == "1":
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
