efa2a7d65eca0cdb032b04ef3fd0ae28f0de00b9
diegomacias-11 <diegojairmacias@gmail.com>
2025-12-01 09:57:52 -0600

    siempre se reenvia al login

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('', include('core.urls')),
     path('', include('comercial.urls')),
     path('', include('clientes.urls')),
