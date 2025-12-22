3081ff519f50aca7379627bf624cd43417ad1986
diegomacias-11 <diegojairmacias@gmail.com>
2025-12-22 11:39:40 -0600

    correccion en modelos

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.apps import apps
 from django.contrib.admin.sites import AlreadyRegistered
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
 
 """
 Autoregistra todos los modelos instalados para que respeten los permisos
@@ -8,7 +10,12 @@ configurados en el admin sin tener que declararlos manualmente.
 Si un modelo ya está registrado (por un ModelAdmin personalizado), se omite.
 """
 
+# Saltar modelos que no queremos mostrar
+_SKIP_MODELS = {ContentType, Session}
+
 for model in apps.get_models():
+    if model in _SKIP_MODELS:
+        continue
     try:
         admin.site.register(model)
     except AlreadyRegistered:
