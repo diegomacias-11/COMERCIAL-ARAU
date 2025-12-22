e5a82aa1daeab3b8cb858aea3c18c3010c87df04
diegomacias-11 <diegojairmacias@gmail.com>
2025-12-22 11:51:24 -0600

    se usa hora local

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,10 @@ from django.contrib.admin.sites import AlreadyRegistered
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sessions.models import Session
 from django.contrib.admin.models import LogEntry
+from django.utils import timezone
+from django.utils.timezone import localtime
+
+from .models import UserSessionActivity
 
 """
 Autoregistra todos los modelos instalados para que respeten los permisos
@@ -21,3 +25,22 @@ for model in apps.get_models():
         admin.site.register(model)
     except AlreadyRegistered:
         pass
+
+# Reemplazar con un ModelAdmin personalizado para mostrar horario local
+try:
+    admin.site.unregister(UserSessionActivity)
+except Exception:
+    pass
+
+
+@admin.register(UserSessionActivity)
+class UserSessionActivityAdmin(admin.ModelAdmin):
+    list_display = ("user", "session_key", "last_seen_local", "ip_address", "user_agent")
+    list_filter = ("user",)
+    search_fields = ("user__username", "session_key", "ip_address", "user_agent")
+    ordering = ("-last_seen",)
+
+    def last_seen_local(self, obj):
+        return localtime(obj.last_seen, timezone.get_current_timezone())
+
+    last_seen_local.short_description = "Último acceso (local)"
